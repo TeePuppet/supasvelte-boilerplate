5868c11f09c2ae487d6ec795f3ed94d7d50d53ad
TeePuppet <silviu@silviu.co.uk>
2024-08-28 20:30:20 +0300

    test buttons

--- src/routes/app/shirts/+page.svelte
+++ src/routes/app/shirts/+page.svelte
@@ -69,17 +69,17 @@ const markAsDone = async (id) => {
 
 	{#each shirts as shirt }
 	<Drawer.Root>
+		<div class="relative">
 		<Drawer.Trigger>
-			<div class="border-2 rounded-lg text-left overflow-hidden">
+			<div class="relative border-2 rounded-lg text-left overflow-hidden">
 				<img src="https://placehold.co/400x400"/>
-				<div class="p-4 flex flex-col gap-2">
-					<h2 class="text-sm font-medium">{shirt.listing_title}</h2>
-					<p class="text-muted-foreground text-sm"><span class="text-foreground font-medium">{shirt.keyword}</span>{#each shirt.tags as tag}
-						<span>, {tag}</span>
-					{/each} </p>
+				<div class="absolute top-2 left-2 px-2 py-1 flex flex-col gap-2 z-10 bg-black/10 rounded">
+					<h2 class="text-sm font-medium">{shirt.keyword}</h2>
 				</div>
 			</div>
 		</Drawer.Trigger>
+			<Button class="absolute top-2 right-2" size="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></svg></Button>
+			<Button class="absolute bottom-3 right-2 bg-green-600" size="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg></Button></div>
 		<Drawer.Portal>
 			<Drawer.Overlay class="fixed inset-0 bg-black/40" />
 			<Drawer.Content class=" flex flex-col fixed bottom-0 left-0 right-0 max-h-[96%] rounded-t-[10px]">
